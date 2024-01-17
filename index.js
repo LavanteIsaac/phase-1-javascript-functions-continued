4f5ebb5c9e9be03ed73b11c1c9a97381b9b7703f
Lavante Cotton <lavanteisaac@gmail.com>
2024-01-17 12:42:10 -0500

    passed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+return (`This Saturday, I want to ${activity}!`);
+
+}
+
+saturdayFun()
+saturdayFun("bathe my dog");
+
+
+function mondayWork(duties = "go to the office"){
+return (`This Monday, I will ${duties}.`)
+}
+mondayWork()
+mondayWork("work from home")
+
+
+function wrapAdjective(){
+    let result = wrapAdjective('*')
+    let emphatic = result("a hard worker")
+    expect(emphatic).to.equal("You are *a hard worker*!")
+  }
+
+        function wrapAdjective(highlight) {
+            return function(adjective) {
+              return `You are ${highlight}${adjective}${highlight}!`;
+            }
+          }
+          
+          const emphatic = wrapAdjective("||");
+          console.log(emphatic("a dedicated programmer"));
